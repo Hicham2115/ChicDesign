9fc177f1b2ed318c34ee7b5efde873d622b4c6ee
hicham <relaxingchannel65@gmail.com>
2026-06-04 23:53:43 +0100

    g

--- lib/db.ts
+++ lib/db.ts
@@ -846,7 +846,7 @@ export const products: Product[] = [
   {
     id: "80",
     name: "Elegant Designer Bag",
-    price: 350,
+    price: 270,
     image: newBag31,
     category: "Bags",
     description: "A stylish and elegant bag for everyday use.",
